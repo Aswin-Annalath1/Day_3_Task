b13e4dcadf89aa07803f65316ec25c4b640655f7
Aswin-Annalath1 <140729100+Aswin-Annalath1@users.noreply.github.com>
2023-08-21 22:24:11 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,15 @@
+let obj1={
+    name:"Person 1",
+    age:5
+}
+let obj2={
+    age:5,
+    name:"Person 1"
+}
+if (obj1.name===obj2.name && obj1.age===obj2.age){
+    console.log("True")
+}
+else {
+    console.log("False")
+}
+
